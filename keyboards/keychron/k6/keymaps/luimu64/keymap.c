7c3082d38d8207edd3dbad2892ef0ce202080354
luimu64 <luimu@luimu.dev>
2023-03-05 18:40:15 +0200

    added copyright notice, and wrote simple readme

--- keyboards/keychron/k6/keymaps/luimu64/keymap.c
+++ keyboards/keychron/k6/keymaps/luimu64/keymap.c
@@ -1,6 +1,7 @@
 /*
 Copyright 2020 Dimitris Mantzouranis
 Copyright 2022 Philip Mourdjis
+Copyright 2023 Leimu Raittinen
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -17,10 +18,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 #include QMK_KEYBOARD_H
 
-// Each layer gets a name for readability, which is then used in the keymap matrix below.
-// The underscores don't mean anything - you can have a layer called STUFF or any other name.
-// Layer names don't all need to be of the same length, obviously, and you can also skip them
-// entirely and just use numbers.
 enum layer_names {
     _DVPFI,
     _DVRAISE,
@@ -131,29 +128,3 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
             {   KC_TRNS,    KC_TRNS,   KC_TRNS,   KC_NO,   KC_NO,   KC_NO,   KC_NO,    KC_NO,   KC_NO,   KC_NO,     KC_RALT,   KC_NO,     KC_NO,     RGB_SAD,   RGB_SPD,   RGB_SAI    }
             }
 };
-
-
-bool dip_switch_update_user(uint8_t index, bool active){
-   /* switch(index){
-        case 0: // macos/windows togggle
-            if(active){ //macos
-                layer_off(_WINDOWS);
-                layer_on(_MAC);
-            }
-            else{ //windows
-                layer_on(_WINDOWS);
-                layer_off(_MAC);
-            }
-        break;
-    }*/
-    return true;
-}
-
-
-void keyboard_post_init_user(void) {
-    // Customise these values to desired behaviour
-    // debug_enable=true;
-    // debug_matrix=true;
-    // debug_keyboard=true;
-    // debug_mouse=true;
-}
